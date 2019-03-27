632f8ffb5013d6ccf72032ccfe7d03be0f649027
cris <cristina@binalogue.com>
2019-03-27 12:43:37 +0100

    esqueleto + CRUD

---
+++ app.js
@@ -0,0 +1,44 @@
+const moviesRef = firebase.database().ref("movies");
+
+// moviesRef.set(["hola",14 ,"fin"]);
+
+// moviesRef.set({
+//     title: "Hulk",
+//     creationDate: new Date().getTime()
+// });
+
+// moviesRef.update({
+//     title: "The Hulk",
+//     description: "bla bla bla.."
+// });
+
+// CRUD (CREATE, READ, UPDATE, DELETE)
+
+function addMovie (data) {
+    return moviesRef.push(data)
+}
+
+function deleteMovie (id) {
+    return moviesRef.child(id).remove()
+}
+
+function updateMovie (id, data) {
+    return moviesRef.child(id).set(data)
+}
+
+function getMovieDetails(id) {  
+    // Detalles de una película
+    return new Promise ((resolve, reject) => {
+        moviesRef.child(id).once("value", data => {
+            resolve(data.val())
+        })
+    })
+       
+}
+
+function getMovies() {
+    // Listado de películas
+    return moviesRef.on("value", data => {
+        console.log("data: ", data.val())
+    })
+}
